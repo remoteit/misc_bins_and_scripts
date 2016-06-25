5568cf3e03b0d6aabac7ef87cb3858c76c56bffa
Digital-Larry <faultline1989@yahoo.com>
2016-06-25 05:56:27 -0700

    Update update_pi.sh
    fixed typo

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd-1.3-06g.deb -O /tmp/weaved-g.deb
-dpkg -i /tmp/weaved-d.deb
+SOURCE=https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd-1.3-06g.deb
+wget "$SOURCE" -O /tmp/weaved-g.deb
+dpkg -i /tmp/weaved-g.deb

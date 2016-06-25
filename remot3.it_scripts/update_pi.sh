c1844ac05df04ca103481162350bc11197ffe5fb
Digital-Larry <faultline1989@yahoo.com>
2016-06-25 12:46:22 -0700

    Update update_pi.sh
    Updated to use version i instead of g.

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SOURCE=https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd-1.3-06g.deb
-wget "$SOURCE" -O /tmp/weaved-g.deb
-dpkg -i /tmp/weaved-g.deb
+SOURCE=https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd-1.3-06i.deb
+wget "$SOURCE" -O /tmp/weaved-i.deb
+dpkg -i /tmp/weaved-i.deb

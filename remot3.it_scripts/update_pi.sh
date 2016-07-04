a8183d03031c69f278db36364ad1f0d1fe757327
James Fox <jim@weaved.com>
2016-07-03 22:32:57 -0700

    Update update_pi.sh
    fixed weavedconnectd-1.3-06j.deb to be weavedconnectd_1.3-06j.deb    the file has an _ character after weavedconnectd

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SOURCE=https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd-1.3-06j.deb
+SOURCE=https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd_1.3-06j.deb
 wget "$SOURCE" -O /tmp/weaved-j.deb
 dpkg -i /tmp/weaved-j.deb

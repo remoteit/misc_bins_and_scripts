74f2923067f8d0500eb4f6d25e79ac476332c082
Digital-Larry <faultline1989@yahoo.com>
2016-07-04 07:08:10 -0700

    Update update_pi.sh
    updated link from version j to k

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SOURCE=https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-06/weavedconnectd_1.3-06j.deb
-wget "$SOURCE" -O /tmp/weaved-j.deb
-dpkg -i /tmp/weaved-j.deb
+SOURCE=https://github.com/weaved/installer/raw/Version-1.3-06k/Raspbian%20deb/1.3-06/weavedconnectd_1.3-06k.deb
+wget "$SOURCE" -O /tmp/weaved-k.deb
+dpkg -i /tmp/weaved-k.deb

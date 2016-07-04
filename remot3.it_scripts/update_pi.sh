3418e12b16de5c12876abf654ca3c64191bf266f
Digital-Larry <faultline1989@yahoo.com>
2016-07-04 14:39:22 -0700

    Update update_pi.sh
    Added weavedstart.sh to the end.

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -2,3 +2,4 @@
 SOURCE=https://github.com/weaved/installer/raw/Version-1.3-06k/Raspbian%20deb/1.3-06/weavedconnectd_1.3-06k.deb
 wget "$SOURCE" -O /tmp/weaved-k.deb
 dpkg -i /tmp/weaved-k.deb
+weavedstart.sh

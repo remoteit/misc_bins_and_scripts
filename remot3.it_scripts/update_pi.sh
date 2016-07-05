58edd8039dc7ff9e97d2c4aba916fb68a00a08f2
Digital-Larry <faultline1989@yahoo.com>
2016-07-05 09:32:08 -0700

    Update update_pi.sh
    Added nohup to dpkg -i line, to prevent interruption.

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SOURCE=https://github.com/weaved/installer/raw/Version-1.3-06k/Raspbian%20deb/1.3-06/weavedconnectd_1.3-06k.deb
 wget "$SOURCE" -O /tmp/weaved-k.deb
-dpkg -i /tmp/weaved-k.deb
+nohup dpkg -i /tmp/weaved-k.deb
 weavedstart.sh

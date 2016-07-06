07d10954099b73516b8489f943f06f5612704015
James Fox <jim@weaved.com>
2016-07-06 08:30:34 -0700

    Update update_pi.sh

--- remot3.it_scripts/update_pi.sh
+++ remot3.it_scripts/update_pi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SOURCE=https://github.com/weaved/installer/raw/Version-1.3-06k/Raspbian%20deb/1.3-06/weavedconnectd_1.3-06k.deb
 wget "$SOURCE" -O /tmp/weaved-k.deb
-nohup dpkg -i /tmp/weaved-k.deb
-weavedstart.sh
+#nohup dpkg -i /tmp/weaved-k.deb
+resp=$(nohup dpkg -i /tmp/weaved-k.deb 2>&1  &); echo

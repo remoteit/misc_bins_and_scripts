afe40c3d6b258da7aa2805937699f8fe031e147c
Digital-Larry <faultline1989@yahoo.com>
2017-07-17 21:22:10 +0000

    Added remot3.it exit codes and clear column.

--- remot3.it_scripts/dropbox/uploadsyslog.sh
+++ remot3.it_scripts/dropbox/uploadsyslog.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+TOOL_DIR="/usr/bin"
+
+#if you need to update status in log running process use the following (not more than once every 30 seconds)
+#task_notify.sh 1 $1 "Job at stage x"
+
+# Clear all status columns A-E in remot3.it portal
+
+ret=$(${TOOL_DIR}/task_notify.sh a $1 $2 "")
+#===========================================================
 logfile="/tmp/$0.log"
 HOMEDIR="/home/pi"
 
@@ -17,9 +26,12 @@ fi
 ./dropbox_uploader.sh  upload /var/log/messages  "$da"/"$dtm"_messages | grep "DONE"
 
 if [ ! $? ]; then
-echo "Error uploading logfile" >> "$logfile"
+    echo "Error uploading logfile" >> "$logfile"
+    ret=$(${TOOL_DIR}/task_notify.sh 2 $1 $2 "Upload failed.")
 else
-echo "Success uploading logfile" >> "$logfile"
+    echo "Success uploading logfile" >> "$logfile"
+    ret=$(${TOOL_DIR}/task_notify.sh 1 $1 $2 "Upload succeeded.")
+    ./dropbox_uploader.sh  list "/$da"  >> "$logfile"
 fi
 
-./dropbox_uploader.sh  list "/$da"  >> "$logfile"
+

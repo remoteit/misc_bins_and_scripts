209be9574aad07a3cb5cad2ef2961c4f5ce25fab
Digital-Larry <faultline1989@yahoo.com>
2017-07-17 22:37:08 -0700

    Update installDropBox.sh

--- remot3.it_scripts/dropbox/installDropBox.sh
+++ remot3.it_scripts/dropbox/installDropBox.sh
@@ -12,6 +12,45 @@ cd /home/pi
 git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
 dropbox_api_token=""
 
+###############################
+# Main program starts here    #
+###############################
+#
+# Must have 3 parameters
+#
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    usage
+    exit 1
+fi
+
+jobid=$1
+shift
+api_server=$1
+shift
+short_code=$1
+
+#
+if [ $DEBUG_ON -gt 0 ]; then
+    echo "$0 called with jobid $jobid api_server $api_server and shortcode $short_code" >> ${DEBUG_DIR}/$0
+fi
+
+command=$(translate $short_code)
+
+if [ "$?" -gt 0 ]; then
+    echo "[Fail] translate short code $short_code failed"
+    exit 1
+fi    
+
+if [ $DEBUG_ON -gt 0 ]; then
+    echo "Translated short code to $command" >> ${DEBUG_DIR}/$0
+fi
+
+#
+# Must use eval to correctly expand command
+#
+eval set -- ${command}
+
+
 ################################################
 # parse the flag options (and their arguments) #
 ################################################
@@ -53,4 +92,5 @@ echo "y" >> db_installer.tmp
 # rm db_installer.tmp
 
 ret=$(${TOOL_DIR}/task_notify.sh a $1 $2 "Done.")
+ret=$(${TOOL_DIR}/task_notify.sh b $1 $2 "$dropbox_api_token")
 ret=$(${TOOL_DIR}/task_notify.sh 1 $1 $2 "Done.")

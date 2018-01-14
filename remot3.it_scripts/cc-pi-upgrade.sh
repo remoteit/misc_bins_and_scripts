ddc607cd3355298cae60ddf1ab793674d8ef39af
Gary <faultline1989@yahoo.com>
2018-01-14 06:42:25 -0800

    Update

--- remot3.it_scripts/cc-pi-upgrade.sh
+++ remot3.it_scripts/cc-pi-upgrade.sh
@@ -13,13 +13,19 @@ dpkg --purge weavedconnectd-clare
 if [ -f weavedconnectd_1.3-07c_armhf.deb ]; then
     rm weavedconnectd_1.3-07c_armhf.deb
 fi
-
+# download new deb pkg
 wget https://github.com/weaved/installer/raw/master/Raspbian%20deb/1.3-07/weavedconnectd_1.3-07c_armhf.deb
-dpkg -i weavedconnectd_1.3-07c_armhf.deb
+
 # get Clare version of rmt3 conf file
 wget https://github.com/weaved/installer/raw/master/weaved_software/enablements/rmt3.pi
+# check MD5 sums for any problem
+echo "3e9b3fdd933400677c465d49032b7db1  weavedconnectd_1.3-07c_armhf.deb" > /tmp/wmd5.txt
+echo "6799810c2e8846319c8c71ca0d041eaf rmt3.pi" >> /tmp/wmd5.txt
+DLOK=$(md5sum -c /tmp/wmd5.txt)
+# everthing checks out, so proceed
 mv rmt3.pi /usr/share/weavedconnectd/conf
-# now install rmt3 service
+# now install newe deb pkg, then rmt3 service
+dpkg -i weavedconnectd_1.3-07c_armhf.deb
 cp /usr/bin/remot3it_register /root
 sed s/USERNAME=\"\"/USERNAME=\"$NAME\"/g < /usr/bin/remot3it_register > /tmp/rr.sh
 sed s/REPLACE_AUTHHASH/$AUTH/g < /tmp/rr.sh > /tmp/rr2.sh
